00b47cc1f93fe29f06a6e2138ebea3e1d5b7a8e9
Douglas Oliveira <douglas.toliveira.dev@gmail.com>
2025-02-08 15:52:38 -0300

    Projeto criado, config server finalizado

---
+++ VendaService/src/main/java/com/douglas/vendas/online/VendaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.douglas.vendas.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaServiceApplication.class, args);
+	}
+
+}
