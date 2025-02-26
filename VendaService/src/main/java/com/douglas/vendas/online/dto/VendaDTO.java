864981bbfcfc1ffb1029505fd8b4237abae69e52
Douglas Oliveira <douglas.toliveira.dev@gmail.com>
2025-02-25 23:41:48 -0300

    Venda repositorio, DTO e BuscaVenda

--- VendaService/src/main/java/com/douglas/vendas/online/dto/VendaDTO.java
+++ VendaService/src/main/java/com/douglas/vendas/online/dto/VendaDTO.java
@@ -1,37 +1,27 @@
-package com.douglas.vendas.online.domain;
+package com.douglas.vendas.online.dto;
 
-import java.math.BigDecimal;
+import java.time.Instant;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
-public class Produto {
-
-	private String id;
-
+public class VendaDTO {
+	
 	@NotNull
 	@Size(min = 2, max = 10)
 	private String codigo;
-
+	
 	@NotNull
-	@Size(min = 1, max = 50)
-	private String nome;
-
+	private String clienteId;
+	
 	@NotNull
-	@Size(min = 1, max = 50)
-	private String descricao;
-
-	@NotNull
-	private BigDecimal valor;
-
+	private Instant dataVenda;
 }
